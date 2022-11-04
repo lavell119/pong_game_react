77785985ba7a8049b5bf94ccb3aa9b39e630d8fb
Lee <leewhiz119@gmail.com>
2022-11-04 18:24:47 -0400

    formatted account-display

--- src/components/AccountInformation.js
+++ src/components/AccountInformation.js
@@ -2,6 +2,7 @@ export default function AccountInformation() {
     return (
         <>
         <div className="display-element account-information">
+                <div class="ai-row">
                 <div className="personal-info">
                     <h4>Personal Info</h4>
                         
@@ -16,18 +17,43 @@ export default function AccountInformation() {
                         </div>
                     </div>
                 </div>
+                </div>
+                <div class="ai-row">
                 <div className="personal-info">
                     <h4>Account Status</h4>
-                    <div className="widget">
+                    <div className="widget between">
                         <div className="personal-info-text bold">
-                            <div className="nick-key">Nick:</div>
-                            <div className="email-key">Email:</div>
+                            <div>Rating Info</div>
+                            <br />
+                            <div>Ranking</div>
                         </div>
                         <div className="personal-info-text">
-                            <div className="nick-key">Username</div>
-                            <div className="email-key">superman@gmail.com</div>
+                            <div></div>
+                            <br />
+                            <div>mlll</div>
                         </div>
                     </div>
+                </div>
+                <div className="personal-info">
+                    <div className="widget between">
+                        <div className="personal-info-text bold">
+                            <div>uBTC</div>
+                            <div>uBCH</div>
+                        </div>
+                        <div className="personal-info-text">
+                            <div>0</div>
+                            <div>0</div>
+                        </div>
+                    </div>
+                    <div className="widget between">
+                        <div className="personal-info-text bold">
+                            <div>Krill</div>
+                        </div>
+                        <div className="personal-info-text">
+                            <div>3219</div>
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
             </>
