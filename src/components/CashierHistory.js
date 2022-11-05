34522ca3d732f03d68310c5a263d255787c90a48
Lee <leewhiz119@gmail.com>
2022-11-05 11:45:12 -0400

    formatted inputs/selects for cashierHistory

--- src/components/CashierHistory.js
+++ src/components/CashierHistory.js
@@ -3,12 +3,19 @@ export default function CashierHistory() {
         <div className="history">
             <div className="history-row-1">
                 <div className="history-row-1-part-1">
-                    <input></input>
-                    <input></input>
+                <select class="cash-history-select-1" placeholder="BTC">
+                    <option value="uBTC">uBTC</option>
+                    <option value="uBCH">uBCH</option>
+                </select>
+                <select class="cash-history-select-2" placeholder="Deposit">
+                    <option value="uBTC">Deposit</option>
+                    <option value="uBCH">Withdraw</option>
+                    <option value="uBTC">Transfer</option>
+                </select>
                 </div>
                 <div className="history-row-1-part-2">
-                    <input></input>
-                    <input></input>
+                    <input placeholder="6/10/2022"></input>
+                    <input placeholder="5/11/2022"></input>
                 </div>
             </div>
         </div>
