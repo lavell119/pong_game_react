805eed92613f5ed3e4340984307f4fe3cab2c541
Lee <leewhiz119@gmail.com>
2022-11-13 18:51:13 -0500

    added footer widget images

--- src/components/FooterWidget.js
+++ src/components/FooterWidget.js
@@ -1,9 +1,15 @@
+import chat from './images/comment4.png'
+import trophy from './images/first-place-trophy.png'
+import table from './images/pokertable.png'
+
+
+
 export default function FooterWidget () {
     return (
         <div className="footer-widget">
-            <div className="ftr-widget">0</div>
-            <div className="ftr-widget">0</div>
-            <div className="ftr-widget">0</div>
+            <div className="ftr-widget"><img src={chat}></img>0</div>
+            <div className="ftr-widget"><img src={trophy}></img>0</div>
+            <div className="ftr-widget"><img src={table}></img>0</div>
         </div>
     )
 }
