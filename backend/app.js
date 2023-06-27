61548e74073e145d4a967d3d9b883088fd9b5f34
Lee <leewhiz119@gmail.com>
2023-06-27 16:38:48 -0400

    started working on /signup post request handler in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,32 @@
+const User = require('../models/userModel')
 const express = require('express')
 
 const app=express()
 
+mong_URI="mongodb+srv://Lavell119:<password>@cluster0.19jswsh.mongodb.net/?retryWrites=true&w=majority"
+
 app.listen(4000, (err)=>{
     console.log("listening on port 4000")
+
 })
 
 app.post('/login', (req,res)=>{
 
 })
 
-app.post('/signup', (req,res)=>{
+app.post('/signup', async function(req,res){
+    const {email, password} = req.body
+
+    try {
+        const User = await User.create({email, password})
+        res.status(200).json(user)
+    }catch(err) {
+        res.status(400).send(err)
+    }
 
 })
+
+
+
+
+
