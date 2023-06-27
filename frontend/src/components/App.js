b175484722d36e66f06503ae18a652c48b6282d9
Lee <leewhiz119@gmail.com>
2023-06-27 19:12:12 -0400

    installed react-router-dom and started creating routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -7,21 +7,26 @@ import { useSelector } from 'react-redux'
 import FooterWidget from './FooterWidget.js'
 import Table1 from './tables/Table1.js'
 
+import { BrowserRouter, Route, ROutes } from 'react-router-dom'
+
+
 
 
 function App() {
   const middleThirdToggle=useSelector(state => state.middleThirdOptions)
   return (
     <>
-  <div className="top-third">
-  <Header />
-  <Seperator />
-  <DisplayHeader />
-  </div>
-  <MiddleThird />
-  <div className="bottom-third">
-    <FooterWidget />
-  </div>
+
+      <div className="top-third">
+      <Header />
+      <Seperator />
+      <DisplayHeader />
+      </div>
+      <MiddleThird />
+      <div className="bottom-third">
+        <FooterWidget />
+      </div> 
+    
     </>
 )
 }
