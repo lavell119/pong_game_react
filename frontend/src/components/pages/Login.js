7bb169aa6c80a92078b12260482f4f8c64566109
Lee <leewhiz119@gmail.com>
2023-06-28 12:37:55 -0400

    created password and email state and set inputs to update both with user input

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,8 +1,30 @@
  import React from 'react'
+ import {useState} from 'react'
+
  
  export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const changeEmail = (e) =>{
+    e.preventDefault()
+    setEmail(e.target.value)
+  }
+
+  const changePassword = (e) =>{
+    e.preventDefault()
+    setPassword(e.target.value)
+  }
+
    return (
-     <h3>Login</h3>
+     <form class="login">
+        <label>Email</label>
+        <input type="email" name="email" value={email} onChange={changeEmail}></input>
+        <label>Password</label>
+        <input type="password" name="password" value={password} onChange={changePassword}></input>
+        {email}
+        {password}
+     </form>
    )
  }
  
