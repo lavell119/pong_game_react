8b71c965501829a1b3b95b76f559f1b6ee33c527
Lee <leewhiz119@gmail.com>
2023-06-28 12:55:06 -0400

    added login function to send request to /login api route and added it to form onSubmit

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -16,14 +16,25 @@
     setPassword(e.target.value)
   }
 
+  const login = async (e, email, password) => {
+    e.preventDefault()
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({email, password})
+    })
+    console.log('request sent')
+  }
+
    return (
-     <form class="login">
+     <form onSubmit={login} class="login">
         <label>Email</label>
         <input type="email" name="email" value={email} onChange={changeEmail}></input>
         <label>Password</label>
         <input type="password" name="password" value={password} onChange={changePassword}></input>
         {email}
         {password}
+        <button>Login</button>
      </form>
    )
  }
