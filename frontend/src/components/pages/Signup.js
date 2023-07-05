013b2089035d891bf404fa68fb88adb358f33d00
Lee <leewhiz119@gmail.com>
2023-07-05 16:12:38 -0400

    succesfully rendered error message inside Signup form

--- frontend/src/components/pages/Signup.js
+++ frontend/src/components/pages/Signup.js
@@ -57,6 +57,7 @@ export default function Signup() {
             </div>  
           </div>
           <button>Signup</button>
+          {error && <div className="error">{error}</div>}
         </div>       
      </form>
      </div>
