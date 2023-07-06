d39a3968bd34bb3a53ba0d36d1261c61e7bbec30
Lee <leewhiz119@gmail.com>
2023-07-05 23:11:14 -0400

    updated-react-router-dom

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,15 +4,11 @@ import Seperator from './Seperator.js'
 import DisplayHeader from './DisplayHeader.js'
 import MiddleThird from './MiddleThird.js'
 import { useSelector } from 'react-redux'
-import FooterWidget from './FooterWidget.js'
-import Table1 from './tables/Table1.js'
 import Home from './pages/Home.js'
 import Signup from './pages/Signup.js'
 import Login from './pages/Login.js'
 import Pong_Table from './PongComponents/Pong_Table.js'
-import AddTableButton from './PongComponents/AddTableButton.js'
-
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Routes, Navigate } from 'react-router-dom'
 
 
 
@@ -25,8 +21,9 @@ function App() {
          <button>{user}</button>
 
     <BrowserRouter>
-      <Switch>
+      <Routes>
       <Route exact path="/">
+        
         <Home />
       </Route>
       <Route exact path ="/login">
@@ -38,7 +35,7 @@ function App() {
       <Route exact path ="/table">
         <Pong_Table />
       </Route>
-      </Switch>
+      </Routes>
     </BrowserRouter>
     
 </>
